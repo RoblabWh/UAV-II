23881177f9f66524416663c9ead162d5c6a7ca8f
Gerhard Senkowski <gerhard.senkowski@freenet.de>
2019-08-16 16:00:48 +0200

    Tello Datensatz und Test

--- DJITello/main.py
+++ DJITello/main.py
@@ -13,15 +13,22 @@ def main():
     write_to_log("Initialise Tello Drone")
     drone = tello.Tello('', 8889)
     cnn_navigation = tf.keras.models.load_model("./model.h5")
-    eta = 30.0
+    eta = 10.0
 
     tmp_stream = videoStream.videostream(drone)
 
     #ToDO: In den Controller auslagern.
     drone.send_command("takeoff")
+    drone.send_command("cw 0")
+    time.sleep(7)
+    drone.send_command("up 50")
+    time.sleep(5)
+    drone.send_command("speed 20")
+    
     while True:
         try:
-            frame = cv2.resize(drone.read(), (101, 101))
+            #time.sleep(1)
+            frame = cv2.resize(drone.read(), (101, 101))/255.0
             # objects = self.cnn_object_detection.predict(frame)
             # ToDo: Object Detection should override the IMU Data
             control_data_propability = cnn_navigation.predict(np.expand_dims(frame, axis=0))
@@ -29,19 +36,26 @@ def main():
             right_propability = control_data_propability[0][2]
 
             tmp_stream.setLines(left_propability, control_data_propability[0][1], right_propability)
-            propability = left_propability - right_propability
-            yaw_angle = propability * eta * 10
-            if yaw_angle > 0:
-                drone.send_command("cw " + str(int(yaw_angle)))
-                print("cw " + str(int(yaw_angle)))
-            else:
-                drone.send_command("ccw " + str(int(yaw_angle * (-1))))
-                print("ccw " + str(int(yaw_angle * (-1))))
+            propability = left_propability / 2 - right_propability
+            yaw_angle = propability * eta
+            #if yaw_angle > 0:
+                #drone.send_command("cw " + str(int(yaw_angle)))
+                #print("cw " + str(int(yaw_angle)))
+            #else:
+                #drone.send_command("ccw " + str(int(yaw_angle * (-1))))
+                #print("ccw " + str(int(yaw_angle * (-1))))
+            rc_drone_command = "rc 0 " + str(int(control_data_propability[0][1] * 40)) + " 0 " + str(int(propability * 50))
+            drone.send_command(rc_drone_command)
+            print(rc_drone_command)
+            #drone.send_command("rc 0 0 0 20")
         except:
             print("Unexpected error:", sys.exc_info()[0])
             #raise
 
-        # self.tello_drone.send_command("forward 20")
+        #drone.send_command("forward 100")
+        #time.sleep(1)
+        #drone.send_command("command")
+        #print("forward 30")
 
 
 if __name__ == "__main__":
