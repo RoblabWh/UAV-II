f849a1f785dfdbde071034177714bcd56a29ef3e
DominikSlomma <50211528+DominikSlomma@users.noreply.github.com>
2020-05-28 18:45:46 +0200

    Add files via upload

--- inference/operator_network/main.py
+++ inference/operator_network/main.py
@@ -6,6 +6,7 @@ sys.path.append(os.getcwd() + '/user_interface')
 #sys.path.append(os.getcwd() + '/network_interface')
 sys.path.append('../network_interface')
 sys.path.append(os.getcwd() + '/joystick')
+sys.path.append(os.getcwd() + '/navigation')
 #sys.path.append(sys.path + "/tello")
 #print(sys.path)
 import drone
@@ -13,23 +14,16 @@ import joystick
 import ui
 import network
 import threading
+import ai
 
 def main():
     tello = drone.drone()
+    nav = ai.ai(tello)
     joy = joystick.joystick(tello)
-    vplayer = ui.UI(tello, joy)
+    vplayer = ui.UI(tello, joy, nav)
     net_send = threading.Thread(target=network.video_server, args=(tello,))
     net_send.start()
     vplayer.root.mainloop()
-    """a = []
-    print(len(a))
-    a.append(('192.168.178.154', 47765))
-    a.append(('192.168.178.154', 47765))
-    a.append(('192.168.178.154', 47765))
-    a.append(('192.168.178.154', 47765))
-    a.append(('192.168.178.154', 47765))
-    print(a[0][0])"""
-
 
 if __name__ == "__main__":
     main()
