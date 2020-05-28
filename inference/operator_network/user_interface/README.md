13e2825f0ba94435813ea6876fe5872fbcc0298a
DominikSlomma <50211528+DominikSlomma@users.noreply.github.com>
2020-05-28 18:43:58 +0200

    Add files via upload

--- inference/operator_network/user_interface/README.md
+++ inference/operator_network/user_interface/README.md
@@ -1,1 +1,4 @@
-# ToDo
+# UAV II - Einsatz von KI im Gefahrenbereich
+## Operator Network
+### User Interface
+Stellt eine Grafische Benutzeroberfläche bereit die den Benutzer unterstützt
--- inference/operator_network/user_interface/ui.py
+++ inference/operator_network/user_interface/ui.py
@@ -12,17 +12,20 @@ from multiprocessing import Process
 class UI:
     """ Wrapper class to enable the GUI """
 
-    def __init__(self, drone, joy):
+    def __init__(self, drone, joy, ai):
         """ Init objectparameter """
         self.drone = drone
+        self.ai = ai
         self.frame = None
         self.video_thread = None
         self.stopEvent = None
         self.isFullscreen = False
+        self.isMImg = False
         self.isRecord = False
         self.joy = joy
         #self.joy_thread = Process(target=self.joy.run)
         self.joy_thread = threading.Thread(target=self.joy.run)
+        self.ai_thread = threading.Thread(target=self.ai.run)
         # initilaize record
         self.fps = 35
         self.fourcc = cv.VideoWriter_fourcc(*'XVID')
@@ -31,10 +34,14 @@ class UI:
         # initialize the root window and image panel
         self.root = tki.Tk()
         self.panel = None
+        #self.root.geometry("300x300")
 
         # create buttons, checkboxes, etc.
         self.btn_fullscreen = tki.Button(self.root, text="Fullscreen einschalten", command=self.setFullscreen)
         self.btn_fullscreen.pack(side="bottom", fill="both", expand="no", padx=10, pady=5)
+
+        self.btn_manipulatedImg = tki.Button(self.root, text="Activate visualisation KI", command=self.showManipulatedImg)
+        self.btn_manipulatedImg.pack(side="bottom", fill="both", expand="no", padx=10, pady=5)
 
         self.btn_record = tki.Button(self.root, text="Record einschalten", command=self.toggleRecord)
         self.btn_record.pack(side="bottom", fill="both", expand="no", padx=10, pady=5)
@@ -49,27 +56,43 @@ class UI:
             start a thread that constanly pools the video sensor for the most recently read frame
         """
         self.stopEvent = threading.Event()
-        self.video_thread = threading.Thread(target=self.videoLoop, args=())
+        self.video_thread = threading.Thread(target=self.videoLoop)
         self.video_thread.start()
 
         #set a callback to handle when the window is closed
-        self.root.wm_title("Tello Controller")
+        self.root.wm_title("UAV - II")
         self.root.wm_protocol("WM_DELETE_WINDOW", self.onClose)
 
         # the sending_command will send command to tello every 5 seconds
         self.periodicSending_thread = threading.Thread(target=self._sendingCommand)
 
+        # use a joystick when something is available
+        self.joy_thread = self.joy_thread = threading.Thread(target=self.joy.run)
+        self.joy_thread.start()
+
+    def showManipulatedImg(self):
+        self.btn_manipulatedImg['text'] = "deactivate visualisation KI"
+        self.isMImg = True
+
+
     def operatorControl(self):
         if(self.btn_ai['text'] == "AI einschalten"):
-            #print("Joystick einschalten")
+            self.btn_ai['text'] = "AI ausschalten"
+            # print("Joystick ausgeschaltet")
+            self.joy.turnoff()
+            self.joy_thread.join()
+            print("AI eingeschaltet")
+            self.ai_thread = threading.Thread(target=self.ai.run)
+            self.ai_thread.start()
+
+
+        else:
+            # print("Joystick einschalten")
+            self.ai.stop()
+            self.ai_thread.join()
+            print("AI ausgeschaltet")
             self.joy_thread = self.joy_thread = threading.Thread(target=self.joy.run)
             self.joy_thread.start()
-            #self.drone.set_joystick(True)
-            self.btn_ai['text'] = "AI ausschalten"
-            # AI aktivieren
-        else:
-            #print("Joystick ausgeschaltet")
-            self.joy.turnoff()
             self.btn_ai['text'] = "AI einschalten"
             # AI deaktivieren
 
@@ -126,13 +149,14 @@ class UI:
         #frame = self.frame
         width, height = frame.shape[:2]
         #print(height)
-        width +=40
+        #self.root.geometry("{}x{}".format(height, width))
+        width += 150
         height = 20
         #print(height)
         bat = self.drone.getBat()
-        text.append("bat: {}".format(bat))
+        text.append("{:>4}{:>3}".format('bat:', bat))
         h = self.drone.getH()
-        text.append("h: {}".format(h))
+        text.append("{:>4}{:>3}".format('h:', h))
         for i in range(0, len(text)):
             cv.putText(frame, text[i], (width, height), cv.FONT_HERSHEY_SIMPLEX, 0.75, color, 2)
             height += 20
@@ -151,15 +175,26 @@ class UI:
             #print("gestartet")
             while not self.stopEvent.is_set():
                 system = platform.system()
+
                 #print("after system")
                 #read the frame for the GUI show
                 self.frame = self.drone.getFrame()
-                frame = self.frame.copy()
-                if(frame is None or frame.size == 0):
+                #print(self.drone.getFrame())
+                if(self.frame is None or self.frame.size == 0):
                     continue
 
+                frame = self.frame.copy()
                 frame = self._getFrame(frame)
+                if(self.isMImg):
+                    mImg = self.drone.getManipulatedFrame()
+                    if(mImg is not None):
 
+                        cv.namedWindow('Support Frame')
+                        cv.imshow("Support Frame", mImg)
+                        if cv.waitKey(1) & 0xFF == ord('q'):
+                            self.isMImg = False
+                            self.btn_manipulatedImg['text'] = "Activate visualisation KI"
+                            cv.destroyAllWindows()
                 if(self.isFullscreen):
                     #print("drin")
                     cv.namedWindow("window", cv.WND_PROP_FULLSCREEN)
@@ -184,38 +219,42 @@ class UI:
                         self._updateGUI(image)
                     else:
                         #print("start Thread in videoLoop")
-                        thread_tmp = threading.Thread(target=self.updateGUI, args=(image,))
-                        thread_tmp.start()
+                        #thread_tmp = threading.Thread(target=self.updateGUI, args=(image,))
+                        #thread_tmp.start()
                         time.sleep(0.03)
                 self._record(20)
+                #print("try your look")
         except RuntimeError:
             print("[INFO] caught a RuntimeError")
+        except Exception as e:
+            print("Exception: {}".format(e))
 
-        #print("videoLoop ende")
+        print("videoLoop ende !!!!!!!!!!!")
 
     def _updateGUI(self, image):
         """
         Main operation to initial the object of image and update the GUI panel
         """
         image = ImageTk.PhotoImage(image)
-        if (self.panel is None):
-            self.panel = tki.Label(image=image)
-            self.panel.image = image
-            self.panel.pack(side="left", padx=10, pady=10)
-        else:
-            self.panel.configure(image=image)
-            self.panel.image = image
+        if(not self.stopEvent.is_set()):
+            if (self.panel is None):
+                self.panel = tki.Label(image=image)
+                self.panel.image = image
+                self.panel.pack(side="left", padx=10, pady=10)
+            else:
+                self.panel.configure(image=image)
+                self.panel.image = image
 
     def _sendingCommand(self):
         """
-        start a while loop that sends 'command' to tello every 5 second
+        start a while loop that sends 'command' to tello every 10 second
         """
 
         while not self.stopEvent.is_set():
             self.drone.send_command('command')
             time.sleep(10)
             #print("gesendet")
-        #print("sending ende")
+        print("Periodic Sending ende")
 
     def onClose(self):
         """
@@ -224,9 +263,20 @@ class UI:
         the quit process to continue
         """
         print("[INFO] closing...")
+        #self.drone.__del__()
+
         self.stopEvent.set()
+
+        time.sleep(5)
+        #self.stopEvent.set()
+        if (self.joy.running):
+            self.joy.__del__()
+
         #self.drone.setTerminate()
         self.drone.__del__()
-        del self.drone
+
+
+        #del self.drone
         self.root.quit()
+        #self.root.destroy()
         print("[print end]")
