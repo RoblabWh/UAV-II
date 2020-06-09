1785faefefcee98a817b05926c5b14345d0843e6
DominikSlomma <dominik.10041991@web.de>
2020-06-09 18:29:52 +0200

    Delete ShowAndTellDemo.sh

--- inference/distributor_network/ShowAndTellDemo.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-echo Starting Segmentation...
-terminator -T "Segmentation" -e "python3 ./segmentation/voc_new.py --cmap './segmentation/cmap.npy' --host_ip '172.16.35.164' --port 5555" &
-
-echo Starting PoseDetection...
-terminator -T "PoseDetection" -e "python3 ./pose_detection/pose_detection.py --host_ip '172.16.35.164' --port 5555" &
